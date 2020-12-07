9401cbaf3ddb794d1b856a803db44edf072cb700
Fabrice Toussaint <fabricetoussaint@Fabrices-MacBook-Pro.local>
2020-12-06 20:14:03 -0500

    convert to postgresql db

--- cms/config/database.js
+++ cms/config/database.js
@@ -4,8 +4,14 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'fabricebt_db'),
+        username: env('DATABASE_USERNAME', 'db_user'),
+        password: env('DATABASE_PASSWORD', 'rR_KN3JsxcKrk!YzZMZRYegYompqFZ'),
+        schema: 'public',
+        // filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
         useNullAsDefault: true,
